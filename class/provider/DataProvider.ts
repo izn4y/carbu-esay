e97c5c9b89ef49ffec3ec091be3b498fa1ae3d72
Euphrosine Devon <n4yor93@hotmail.fr>
2022-06-19 19:48:05 +0200

    add jsDoc and compare function

--- class/provider/DataProvider.ts
+++ class/provider/DataProvider.ts
@@ -18,12 +18,24 @@ export class DataProvider {
   private readonly XML_PATH: string;
   private readonly ENCODING: string;
 
+  /**
+   * Creates an instance of DataProvider.
+   * @memberof DataProvider
+   */
+
   constructor() {
     this.station = new StationDto(null, null, null, null, null, null, null);
     this.totalGasStation = [];
     this.XML_PATH = "./PrixCarburants_instantane.xml";
     this.ENCODING = "iso-8859-1";
   }
+
+  /**
+   *
+   * @private
+   * @return {*}  {Promise<string>}
+   * @memberof DataProvider
+   */
 
   private async loadDataDecode(): Promise<string> {
     return new Promise<string>((resolve, reject) => {
@@ -35,17 +47,25 @@ export class DataProvider {
       }
     });
   }
+
+  /**
+   *@public
+   * @param {string} pCity
+   * @return {*}  {Promise<Array <object> >}
+   * @memberof DataProvider
+   */
+
   public async loadDataXml(pCity:string): Promise<Array <object> > {
   
     await this.loadDataDecode().then((result: string) => {
       xml2js.parseString(result, (err, result: any) => {
         
         if (err) console.log(err);
-        this.totalGasStation = [];
+        this.totalGasStation = new Array<object>();
         result.pdv_liste.pdv.forEach((element: any) => {
           this.station = new StationDto(null,null,null,null,null, null,null);
             
-          if (element.ville == pCity) {
+          if (element.ville === pCity || element.ville === pCity.toUpperCase()) {
             this.station.address = element.adresse.toString();
             this.station.locationId = element["$"].id;
             this.station.latitude = element["$"].latitude;
@@ -62,10 +82,27 @@ export class DataProvider {
             this.totalGasStation.push(this.station);
           }
         });
-        // console.log(this.totalGasStation);
+        console.log(this.totalGasStation);
         // console.log(`${this.totalGasStation.length} stations trouvées`);
       });
     });
     return this.totalGasStation;
+  }
+  /**
+   *
+   * @private
+   * @param {*} a
+   * @param {*} b
+   * @return {*}  {string}
+   * @memberof DataProvider
+   */
+  private comparePrice(a: any, b: any) :string {
+    if (a < b) {
+      console.log( a + " < " + b);
+    }
+    if (a > b) {
+      console.log( a + " > " + b);
+    }
+    return ""
   }
 }
--- services/index.ts
+++ services/index.ts
@@ -5,17 +5,34 @@ const router = express.Router();
 let dataProvider: DataProvider = new DataProvider();
 
 /**
- * @Service 
+ * @Service
  * @FetchData
  */
 
 router.post("/search", (request: Request, response: Response) => {
-  dataProvider.loadDataXml("La Roche-sur-Yon").then((stationDto) => {
-    // stationDto.forEach((gasPrice: any) => {
-    //   console.log(gasPrice);
-    // });
-    console.log(stationDto)
-    console.log(`${stationDto.length} stations trouvées`);
+
+  const priceCompare = (a: any, b: any) => {
+    if (a < b) {
+      // console.log( a + " < " + b);
+    }
+    if (a > b) {
+      // console.log( a + " > " + b);
+    }
+  }
+  dataProvider.loadDataXml("La Roche-sur-Yon").then((stationDto: any) => {
+    stationDto.forEach((gasPrice: any) => {
+      // console.log(gasPrice);
+      let i: number = 0;
+      while (i < gasPrice.carburant_price.length) {
+        if (gasPrice.carburant_price[i].nom === "Gazole") {
+          priceCompare(gasPrice.carburant_price[i].valeur, gasPrice.carburant_price[i + 1].valeur);
+          // console.log(gasPrice.carburant_price[i].valeur);
+        }
+        i++;
+      }
+    });
+    //   // console.log(stationDto)
+    //   console.log(`${stationDto.length} stations trouvées`);
   });
 });
 
